165b96b18f309bd2ba37c3542966d94dfe5d81f0
Jhonatan Mejia <quenandres@gmail.com>
2023-09-01 11:53:33 -0500

    Add: nestjs Setup

---
+++ tasksapi/src/app.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TasksModule } from './tasks/tasks.module';
+
+@Module({
+  imports: [MongooseModule.forRoot('mongodb://localhost/tasksdb'), TasksModule],
+})
+export class AppModule {}
